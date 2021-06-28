364ddc19f5bffbd83a721b423493e8867bd8e531
Eduardo Miranda <eduardocdmb@gmail.com>
2021-06-28 19:44:56 -0300

    Correção de bug na responsividade do menu; Tradução de elementos da Agenda

--- src/menu/Menu.js
+++ src/menu/Menu.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import './Menu.css';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    Nav,
+    NavItem
+} from 'reactstrap';
 
 
 
@@ -13,9 +20,68 @@ export default class Menu extends Component {
         this.props.logado = false;
     }
 
-    
+    constructor(props) {
+        super(props);
 
-    render(){
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" color="inverse" light expand="md">
+                    <div className="container-fluid color-nav text-white p-2">
+                        <NavbarToggler onClick={this.toggle} />
+                        <Collapse isOpen={this.state.isOpen} navbar>
+                            <Nav className="ml-auto" navbar>
+                                <NavItem>
+                                    <li className="nav-item">
+                                        <Link className="nav-link active" aria-current="page" to="/agenda">Agenda</Link>
+                                    </li>
+                                </NavItem>
+                                <NavItem>
+                                    <li className="nav-item">
+                                        {!this.props.logado ? <Link className="d-none" to="/grupo">Grupos</Link> :
+                                            <Link className="nav-link active" to="/grupo" >Grupos</Link>}
+                                    </li>
+                                </NavItem>
+
+                                    <li className="nav-item">
+                                        {!this.props.logado ? <Link className="d-none" to="/usuario">Usuários</Link> :
+                                            <Link className="nav-link active" to="/usuario">Usuários</Link>}
+                                    </li>
+
+                                <NavItem>
+                                    <li className="nav-item">
+                                        {!this.props.logado ? <Link className="d-none" to="/evento">Eventos</Link> :
+                                            <Link className="nav-link active" to="/evento">Eventos</Link>}
+                                    </li>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                        <div className="d-flex">
+                            <div>
+                                {!this.props.logado ? <Link className="navbar-brand" to="/login">Login</Link> : <Link className="navbar-brand" to="/login" onClick={() => this.props.logout()}>Logout</Link>}
+
+                            </div>
+                        </div>
+                    </div>
+                </Navbar>
+            </div>
+        );
+    }
+
+
+
+    /*render(){
         return(      
 
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
@@ -55,5 +121,5 @@ export default class Menu extends Component {
             </nav>
             
         )
-    }
+    }*/
 }
